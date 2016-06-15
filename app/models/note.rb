34665c364ed75d0d2c3d236a365dcbabde611613
Philip Tolton <mrtolton@gmail.com>
2016-06-15 16:56:17 -0400

    wip

--- app/models/note.rb
+++ app/models/note.rb
@@ -11,8 +11,16 @@ class Note < ActiveRecord::Base
       end
     end
 
-
     total_hash
+  end
+
+  def self.q_model
+    Note.includes(:values).all.each do |note|
+      next unless note.full_name =~ /US Q-Model/
+      note.quarterly_returns.to_s
+    end
+
+    nil
   end
 
   def returns
@@ -20,11 +28,26 @@ class Note < ActiveRecord::Base
     values_by_year.each do |year, values|
       last_day = values.max
       first_day = values.min
-      change = (first_day.price - last_day.price)/last_day.price * 100
-      totals[year] = change
+      totals[year] = rate_of_change(first_day: first_day, last_day: last_day)
       # puts "#{year} -> #{change} [#{first_day.date} -> #{first_day.price}][#{last_day.date} -> #{last_day.price}]"
     end
     totals
+  end
+
+  def quarterly_returns
+    start_dt = values.max
+    end_dt = values.min
+    start_dates = (start_dt.date..end_dt.date).group_by(&:beginning_of_quarter).keys
+    end_dates = (start_dt.date..end_dt.date).group_by(&:end_of_quarter).keys
+    start_dates.zip(end_dates).each do |quarter|
+      range = (quarter.first..quarter.last)
+      dates = values.find_all{ |value| range.include?(value.date.to_date) }
+      start_dt = dates.min
+      end_dt = dates.max
+      change = rate_of_change(first_day: start_dt, last_day: end_dt)
+      next unless change.abs > 10
+      puts "#{quarter.to_s} -> #{change}"
+    end
   end
 
   private
@@ -33,5 +56,9 @@ class Note < ActiveRecord::Base
     values.group_by do |value|
       value.date.year
     end
+  end
+
+  def rate_of_change(first_day:, last_day:)
+    (first_day.price - last_day.price) / last_day.price * 100
   end
 end
